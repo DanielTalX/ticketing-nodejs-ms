6fde6bb3f213415581472ce970893bb3bdeb37ab
Daniel <dtal@attentigroup.com>
2023-07-29 10:28:53 +0300

    sec22 - ep493-497 - Client 4 - Stripe Payment Form

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import useRequest from '../../hooks/use-request';
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ currentUser, order }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+  const intervalRef = useRef(null);
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payment) => console.log(payment),
+  });
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -10,18 +21,30 @@ const OrderShow = ({ order }) => {
     };
 
     findTimeLeft();
-    const timerId = setInterval(findTimeLeft, 1000);
+    intervalRef.timerId = setInterval(findTimeLeft, 1000);
 
     return () => {
-      clearInterval(timerId);
+      clearInterval(intervalRef.timerId);
     };
   }, [order]);
 
-  if (timeLeft < 0) {
+  if (timeLeft <= 0) {
+    clearInterval(intervalRef.timerId);
     return <div>Order Expired</div>;
   }
 
-  return <div>Time left to pay: {timeLeft} seconds</div>;
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => doRequest({ token: "tok_visa" /*token.id*/ })}
+        stripeKey="pk_test_JMdyKVvf8EGTB0Fl28GsN7YY"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
+  );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
